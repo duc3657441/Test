a8eef11b8af14822bc30f0ffd7dd02f9fbcff5a8
NguyenDinhDuc <nguyendinhduc.1821@gmail.com>
2024-06-02 12:07:20 +0700

    update

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            var imageFolder = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "flowers");
+            // load dữ liệu từ folder train
+            var imageFolder = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "train");
             var files = Directory.GetFiles(imageFolder, "*", SearchOption.AllDirectories);
             var images = files.Select(file => new ImageData
             {
@@ -18,22 +19,24 @@ namespace Test
             });
 
             var context = new MLContext();
+            
+            // convert data to Idataview
             var imageData = context.Data.LoadFromEnumerable(images);
-            var imageDataShuffled = context.Data.ShuffleRows(imageData);
 
+            //shuffle data
+            var imageDataShuffled = context.Data.ShuffleRows(imageData);
             var testTrainData = context.Data.TrainTestSplit(imageDataShuffled, testFraction: 0.2);
 
-            var validationData = context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", 
-                keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
-                .Append(context.Transforms.LoadRawImageBytes("Image", imageFolder, "ImagePath"))
-                .Fit(testTrainData.TestSet)
-                .Transform(testTrainData.TestSet);
 
-            var imagePipeline = context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
-                .Append(context.Transforms.LoadRawImageBytes("Image", imageFolder, "ImagePath"));
-
-            var imagesDataModel = imagePipeline.Fit(testTrainData.TrainSet);
-            var imageDataView = imagesDataModel.Transform(testTrainData.TrainSet);
+            var validationData = context.Transforms.Conversion.MapValueToKey(
+                                        outputColumnName: "LabelKey", 
+                                        inputColumnName: "Label",
+                                        keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
+                                            .Append(context.Transforms.LoadRawImageBytes(outputColumnName: "Image", 
+                                                                                        imageFolder: imageFolder, 
+                                                                                        inputColumnName: "ImagePath"))
+                                            .Fit(testTrainData.TestSet)
+                                            .Transform(testTrainData.TestSet);
 
             var options = new ImageClassificationTrainer.Options()
             {
@@ -43,35 +46,71 @@ namespace Test
                 LearningRate = 0.01f,
                 LabelColumnName = "LabelKey",
                 FeatureColumnName = "Image",
-                ValidationSet = validationData
+                ValidationSet = validationData,
+                ReuseTrainSetBottleneckCachedValues = true,
+                ReuseValidationSetBottleneckCachedValues = true,
+                MetricsCallback = (metrics) => Console.WriteLine(metrics),
             };
 
+            var imagePipeline = context.Transforms.Conversion.MapValueToKey(outputColumnName: "LabelKey", 
+                                                                            inputColumnName: "Label", 
+                                                                            keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
+                                                .Append(context.Transforms.LoadRawImageBytes("Image", imageFolder, "ImagePath"));
+
+            
+            var imageDataView = imagePipeline.Fit(testTrainData.TrainSet).Transform(testTrainData.TrainSet);
+
             var pipeline = context.MulticlassClassification.Trainers.ImageClassification(options)
-                .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+                .Append(context.Transforms.Conversion.MapKeyToValue(outputColumnName: "PredictedLabel"));
 
+            //train model
             var model = pipeline.Fit(imageDataView);
+           
+            //save model
+            context.Model.Save(model, imageDataView.Schema, "model.zip");
 
-            var predictionEngine = context.Model.CreatePredictionEngine<ImageModelInput, ImagePrediction>(model);
 
-            var testImagesFolder = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "test1");
-            var testFiles = Directory.GetFiles(testImagesFolder, "*", SearchOption.AllDirectories);
-            var testImages = testFiles.Select(file => new ImageModelInput
-            {
-                ImagePath = file,
-            });
-            Console.WriteLine(Environment.NewLine);
 
-            var testImageData = context.Data.LoadFromEnumerable(testImages);
 
-            var testImageDataView = imagePipeline.Fit(testImageData).Transform(testImageData);
-            var predictions = model.Transform(testImageDataView);
+            // su dung model
+            //var context = new MLContext();
+            //var modelSave = context.Model.Load("model.zip", out DataViewSchema modelSchema);
+            
+            //var testImagesFolder = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "test1");
+            //var testFiles = Directory.GetFiles(testImagesFolder, "*", SearchOption.AllDirectories);
+            
+            //var testImages = testFiles.Select(file => new ImageModelInput
+            //{
+            //    ImagePath = file, 
+            //});
+            //Console.WriteLine(Environment.NewLine);
 
-            var testPredictions = context.Data.CreateEnumerable<ImagePrediction>(predictions, reuseRowObject: false);
+            //var testImageData = context.Data.LoadFromEnumerable(testImages);
+            //var imagePipeline = context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
+            //    .Append(context.Transforms.LoadRawImageBytes(outputColumnName:"Image", 
+            //                                                 inputColumnName: "ImagePath",
+            //                                                 imageFolder: testImagesFolder));
+            //var testImageDataView = imagePipeline.Fit(testImageData).Transform(testImageData);
+            //var predictions = modelSave.Transform(testImageDataView);
 
-            foreach(var prediction in testPredictions)
-            {
-                Console.WriteLine($"Image: {Path.GetFileName(prediction.ImagePath)}, Predicted Label: {prediction.PredictedLabel}");
-            }
+            //var testPredictions = context.Data.CreateEnumerable<ImagePrediction>(predictions, reuseRowObject: false);
+            //int countLive = 0;
+            //int countSpoof = 0;
+            //foreach (var prediction in testPredictions)
+            //{
+            //    //Console.WriteLine($"Image: {Path.GetFileName(prediction.ImagePath)}, Predicted Label: {prediction.PredictedLabel}");
+            //    if (prediction.PredictedLabel == "live")
+            //    {
+            //        countLive++;
+            //    }
+            //    if (prediction.PredictedLabel == "spoof")
+            //    {
+
+            //        countSpoof++;
+            //    }
+            //}
+            //Console.WriteLine($"Live: {countLive}" );
+            //Console.WriteLine($"Spoof: {countSpoof}");
         }
     }
 }
--- obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ae884afa04a163ee2e2066a077c29a518f9811")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
